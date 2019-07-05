4589b5109710f1bb933cb6d9bb88f20ca7aea48d
joy <joy.lai@zg.com>
2019-07-05 15:49:00 +0800

    fix

--- src/wallet.test.ts
+++ src/wallet.test.ts
@@ -1,6 +1,8 @@
 import '@pefish/js-node-assist'
 import BitcoinWalletHelper from './wallet'
 import assert from 'assert'
+import Remote from '@pefish/js-coin-btc/lib/remote'
+import ErrorHelper from '@pefish/js-error'
 
 declare global {
   namespace NodeJS {
@@ -54,6 +56,50 @@ describe('bitcoinWalletHelper', () => {
       const segwit = walletHelper.getAddressFromPublicKey('0339c1597380ad680f5429e12d4b211d06dcdbd6f435ee1c14e48e326173edec59', `p2sh(p2wpkh)`, mainnet)
       // global.logger.info(segwit)
       assert.strictEqual(segwit, `VsNvKUr8H6y3tmBbLCGiDbdQZxsXBKzjLBh`)
+    } catch (err) {
+      global.logger.error(err)
+      assert.throws(() => {}, err)
+    }
+  })
+
+  it('buildTransaction', async () => {
+    try {
+      const rpc = new Remote({
+        host: `34.80.106.43`,
+        port: 8332,
+        username: ``,
+        password: ``,
+        ssl: false,
+      })
+      const wif = ``
+      const unsignedTxHex = await rpc.request(`createrawtransaction`, [
+        [
+          {
+            txid: `79c4a5c2d08f316e92858189b84b43834f4af2f16c53469d6da478f826356ca9`,
+            vout: 1,
+          }
+        ],
+        {
+          VcVHaAyeHUxfjE5GAmENRBjoCiBkDhQc2yG: 92,
+          Vca9sYiSkW24ypDedBDhrHk8zRUtK7wEMcd: 6.9,
+        }
+      ])
+      const signedTxInfo = await rpc.request(`signrawtransaction`, [
+        unsignedTxHex,
+        [],
+        [
+          wif
+        ]
+      ])
+      if (!signedTxInfo[`complete`]) {
+        throw new ErrorHelper(`build tx error. ${JSON.stringify(signedTxInfo[`errors`])}`)
+      }
+      global.logger.info(`[buildTransaction] 成功构建交易. tx: ${signedTxInfo[`hex`]}`)
+      const txInfo = await rpc.request(`decoderawtransaction`, [
+        signedTxInfo[`hex`],
+      ])
+      global.logger.error(txInfo)
+      await rpc.client.sendRawTransaction(signedTxInfo[`hex`])
     } catch (err) {
       global.logger.error(err)
       assert.throws(() => {}, err)
